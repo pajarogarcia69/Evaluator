86e303d62491c087093892cfe109e1ef8c5d67de
pajarogarcia69 <stevenpajarogarcia@gmail.com>
2023-04-10 18:49:08 -0500

    Add project files.

---
+++ Evaluator.UI.Console/Program.cs
@@ -0,0 +1,7 @@
+﻿using Evaluator.Logic;
+
+Console.WriteLine("Expresions Evaluator");
+var expresion1 = "4*5/(4+6)";
+var expresion2 = "4*(5+6-(8/2^3)-7)-1";
+Console.WriteLine($"{expresion1} = {MyEvaluator.Evaluate(expresion1)}");
+Console.WriteLine($"{expresion2} = {MyEvaluator.Evaluate(expresion2)}");
